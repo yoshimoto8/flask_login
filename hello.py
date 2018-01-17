cf8ec3d1ff1402f11da4f978c4b6825da6944aae
吉本光輝 <yoshi@yoshimotomitsuterunoMacBook-puro.local>
2018-01-17 19:02:08 +0900

    flask login

---
+++ hello.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form.get('username'),
+                        request.form.get('password')):
+            flash("Succesfully logged in")
+            session['username'] = request.form.get('username')
+            return redirect(url_for('welcome'))
+        else:
+            error = "Incorrect username and password"
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/')
+def welcome():
+    if 'username' in session:
+        return render_template('welcome.html', username=session['username'])
+    else:
+        return redirect(url_for('login'))
+
+def valid_login(username, password):
+    if username == password:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    app.debug = True
+    app.secret_key = '\xd1M<n)\xf1\xf9\x08\xe2z\x8ai\x99\xf8\xb8\xf0\xe9\x06\x95"0\x9d\xd2\xf9'
+    app.run(port=3500)
